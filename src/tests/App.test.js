443fa2fd5d2ea042de53ebf45932f6662015e96c
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-04-03 18:50:39 -0300

    Requisito 4
    Cobertura de testes de 45%

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+
+import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testando as funcionalidades da aplicação', () => {
+  it('Testando a página Login', () => {
+    renderWithRouter(<App />);
+
+    const title = screen.getByText(/Login/i);
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const button = screen.getByRole('button');
+
+    expect(title).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  })
 });
