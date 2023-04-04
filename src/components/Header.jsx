c5d1b623f1b353079a7b50b9d41d91eca703ff54
Matheus <maahwiski@gmail.com>
2023-04-04 16:12:43 -0300

    Requisito 8 e 9
    Cria a lógica que redireciona a pessoa a rota /profile quando clica no botão, cria teste pra cobrir 100% da página e refatora o seletor na linha 15 do código de teste da página de login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import HeaderContext from '../context/HeaderContext';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
@@ -13,11 +14,13 @@ function Header({ pathname, title }) {
   } = useContext(HeaderContext);
   return (
     <div>
-      <img
-        src={ profileIcon }
-        alt="profile"
-        data-testid="profile-top-btn"
-      />
+      <Link to="/profile">
+        <img
+          src={ profileIcon }
+          alt="profile"
+          data-testid="profile-top-btn"
+        />
+      </Link>
       <h1 data-testid="page-title">
         { title }
       </h1>
