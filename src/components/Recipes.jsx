da93121abfe13bf2a5023472f5602980808cf6f7
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-04-04 19:12:05 -0300

    Requisito 20
    Implementação dos botões dos filtros por categoria

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -7,11 +7,14 @@ import { getMeals } from '../services/mealsAPI';
 import { getDrinks } from '../services/drinksAPI';
 
 function Recipes() {
+  const five = 5;
   const twelve = 12;
   const history = useHistory();
   const { pathname } = history.location;
 
   const [recipes, setRecipes] = useState([]);
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     switch (pathname) {
     case '/meals':
@@ -19,30 +22,54 @@ function Recipes() {
         .then((response) => response
           .slice(0, twelve))
         .then(setRecipes);
+      getMeals('c', 'list', 'list', 'meals')
+        .then((response) => response
+          .slice(0, five))
+        .then(setCategories);
       break;
     case '/drinks':
       getDrinks('s', '', 'search', 'drinks')
         .then((response) => response
           .slice(0, twelve))
         .then(setRecipes);
+      getDrinks('c', 'list', 'list', 'drinks')
+        .then((response) => response
+          .slice(0, five))
+        .then(setCategories);
       break;
     default:
       break;
     }
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname]);
   return (
     <div>
-      {
-        recipes.map((recipe, index) => (
-          <RecipesCard
-            key={ index }
-            index={ index }
-            name={ recipe[pathname === '/meals' ? 'strMeal' : 'strDrink'] }
-            image={ recipe[pathname === '/meals' ? 'strMealThumb' : 'strDrinkThumb'] }
-          />
-        ))
-      }
+      <header>
+        <button>All</button>
+        {
+          categories.map((category, index) => (
+            <button
+              key={ index }
+              data-testid={ `${category.strCategory}-category-filter` }
+            >
+              { category.strCategory }
+            </button>
+          ))
+        }
+      </header>
+      <main>
+        {
+          recipes.map((recipe, index) => (
+            <RecipesCard
+              key={ index }
+              index={ index }
+              name={ recipe[pathname === '/meals' ? 'strMeal' : 'strDrink'] }
+              image={ recipe[pathname === '/meals' ? 'strMealThumb' : 'strDrinkThumb'] }
+            />
+          ))
+        }
+      </main>
     </div>
   );
 }
