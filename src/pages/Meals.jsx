c2a2b27edf7fcecd5bccae8c38c822e556fbb04f
ShayLopes <shaytom71@gmail.com>
2023-04-06 13:52:06 -0300

    Ajustes de conflito

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -4,14 +4,13 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Recipes from '../components/Recipes';
 
-function Meals({ history }) {
+function Meals() {
   return (
-    <>
     <div>
       <Header title="Meals" pathname="/meals" />
       <Recipes />
       <Footer />
-    </>
+    </div>
   );
 }
 
