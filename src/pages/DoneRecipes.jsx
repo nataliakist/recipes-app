9e05b85ed7780b0b04098263bad1bd941b9807c0
Matheus <maahwiski@gmail.com>
2023-04-10 15:06:08 -0300

    requisito 44
    cria o card das receitas prontas com seus data-testid

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,8 +1,31 @@
 import Header from '../components/Header';
+import Button from '../components/Button';
+import FoodCard from '../components/FoodCard';
 
 function DoneRecipes() {
   return (
-    <Header title="Done Recipes" />
+    <main>
+      <Header title="Done Recipes" />
+
+      <form>
+        <Button
+          data-testid="filter-by-all-btn"
+          label="All"
+        />
+
+        <Button
+          data-testid="filter-by-meal-btn"
+          label="Meals"
+        />
+
+        <Button
+          data-testid="filter-by-drink-btn"
+          label="Drinks"
+        />
+
+        <FoodCard />
+      </form>
+    </main>
   );
 }
 
